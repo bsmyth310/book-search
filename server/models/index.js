5ff0cbfec458bd600015f3f1be2c86d494780ace
Ben Smyth <118232312+bsmyth310@users.noreply.github.com>
2023-05-10 12:57:10 -0400

    Add files via upload

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,17 @@
-const User = require('./User');
+const { Schema, model } = require('mongoose');
+const bcrypt = require(bcrypt);
+
+//importschema from book.js
+const bookSchema = require('./Book');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        }
+    }
+)
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('.utils/auth');
 
 // import pre-defined definitions and handling functions
 const { typeDefs, resolvers } = require("./schemas");
@@ -16,7 +14,6 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
 })
 
 app.use(express.urlencoded({ extended: true }));
@@ -34,11 +31,15 @@ app.get('/', (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-
+  
   db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  });
-};
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
 
 //app.use(routes);
 //start server
